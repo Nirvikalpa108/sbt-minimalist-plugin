779da2d2afe29ccca667486cd7f2449fee4f077c
Amina Adewusi <amina.adewusi@permutive.com>
2022-07-31 11:41:11 +0100

    adds tasks which take setting values

--- project/MinimalPlugin.scala
+++ project/MinimalPlugin.scala
@@ -1,16 +1,27 @@
 import sbt.AutoPlugin
 import sbt._
 
+import scala.sys.process.Process
+
 object MinimalPlugin extends AutoPlugin { //Autoplugin puts dependencies in the right order for us.
   override def trigger = allRequirements //plugin doesn't require manual trigger
 
   object autoImport {
     val hello = taskKey[Unit]("greeting")
+    val greetingName = settingKey[String]("name to address greeting to")
+    val sayHello = taskKey[Unit]("say greeting")
+    val sayVoice = settingKey[String]("voice to say hello in")
   }
 
   import autoImport._
   override lazy val globalSettings = List(
-    hello := println("Hello World!")
+    greetingName := "Coco",
+    hello := println(s"Hello ${greetingName.value}"),
+    sayVoice := "Fiona",
+    sayHello := {
+      val voice = sayVoice.value
+      Process(s"say -v $voice Hello World").!!
+    }
   )
 }
 
