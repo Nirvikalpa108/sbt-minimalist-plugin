3fb16f8bdeb8123f156e78cd9246418f2e1beec6
Amina Adewusi <amina.adewusi@permutive.com>
2022-07-31 11:29:51 +0100

    adds basic hello world task

--- project/MinimalPlugin.scala
+++ project/MinimalPlugin.scala
@@ -3,5 +3,14 @@ import sbt._
 
 object MinimalPlugin extends AutoPlugin { //Autoplugin puts dependencies in the right order for us.
   override def trigger = allRequirements //plugin doesn't require manual trigger
+
+  object autoImport {
+    val hello = taskKey[Unit]("greeting")
+  }
+
+  import autoImport._
+  override lazy val globalSettings = List(
+    hello := println("Hello World!")
+  )
 }
 
